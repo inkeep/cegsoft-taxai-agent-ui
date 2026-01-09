3c93cb0b3375c46e07a51e7b4efc245dc17760a7
Bryan Lliguicota <Bryan@inkeep.com>
2026-01-09 14:25:23 -0500

    remove default value from agentUrl in inkeep config

--- src/config/inkeep.ts
+++ src/config/inkeep.ts
@@ -1,5 +1,5 @@
 export const inkeepConfig = {
-  agentUrl: import.meta.env.VITE_INKEEP_AGENT_URL ?? "http://localhost:3003/api/chat",
+  agentUrl: import.meta.env.VITE_INKEEP_AGENT_URL,
   agentApiKey: import.meta.env.VITE_INKEEP_AGENT_API_KEY,
   taxesAiApiKey: import.meta.env.VITE_TAXESAI_API_KEY,
 };
